74935e6810e421389c3ed3db5ae36fdd328793da
Alvaro Malvestiti <alvaromalvestiti@gmail.com>
2024-12-19 17:16:52 -0300

    [ADD] json-server

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
 import { Hero } from "../../components/ui/Hero/Hero";
 
 export const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/products");
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.error("Error fetching products: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div>
       <Hero />
