76d2b2bec3747dd0262763c9839bc9dc1a25f444
Alvaro Malvestiti <alvaromalvestiti@gmail.com>
2024-12-19 16:44:28 -0300

    [ADD] react router dom

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { MainLayout } from "./components/Layouts/MainLayout.tsx";
+import "./index.css";
+import { Cart } from "./pages/Cart/Cart.tsx";
+import { Home } from "./pages/Home/Home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/cart", element: <Cart /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
