3218c07fca6d3f8bd5a44e0e36de8e9b6d43e1e0
Alvaro Malvestiti <alvaromalvestiti@gmail.com>
2024-12-23 11:22:20 -0300

    [ADD] checkout page

---
+++ src/pages/Checkout/Checkout.tsx
@@ -0,0 +1,11 @@
+import { Table } from "../../components/ui/Table/Table";
+
+export const Checkout = () => {
+  return (
+    <div>
+      <Table />
+    </div>
+  );
+};
+
+export default Checkout;
