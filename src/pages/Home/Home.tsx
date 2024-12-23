1884b393efb2688847d68b63315d8e22a466b8f2
Alvaro Malvestiti <alvaromalvestiti@gmail.com>
2024-12-23 12:28:55 -0300

    [ADD] react query

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,28 +1,32 @@
-import { useEffect, useState } from "react";
 import { Hero } from "../../components/ui/Hero/Hero";
 import styles from "./Home.module.css";
 import { CardProduct } from "../../components/ui/CardProduct/CardProduct";
 import { getProducts } from "../../service";
-import { Product } from "../../interface";
 import { Toaster } from "sonner";
+import { useQuery } from "@tanstack/react-query";
 
 export const Home = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [error, setError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["products"],
+    queryFn: getProducts,
+  });
 
-  useEffect(() => {
-    getProducts()
-      .then((data) => {
-        setProducts(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
+  // const [products, setProducts] = useState<Product[]>([]);
+  // const [error, setError] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   getProducts()
+  //     .then((data) => {
+  //       setProducts(data);
+  //     })
+  //     .catch(() => {
+  //       setError(true);
+  //     })
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //     });
+  // }, []);
 
   return (
     <>
@@ -31,7 +35,7 @@ export const Home = () => {
       {isLoading && <p>Loading...</p>}
       {error && <p>There was an error</p>}
       <div className={styles.container}>
-        {products.map((product) => (
+        {data?.map((product) => (
           <CardProduct key={product.tail} product={product} />
         ))}
       </div>
